b64dc436bc38c64119a7fb60dfe32d5c59fc93e1
Yassine Diouri <ametric@gmail.com>
2020-04-01 19:45:41 +0200

    More helper code

--- HuaweiMobileServices/utils/AndroidTypes.cs
+++ HuaweiMobileServices/utils/AndroidTypes.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
 
 namespace HuaweiMobileServices.Utils
 {
@@ -15,6 +16,28 @@ namespace HuaweiMobileServices.Utils
         public static string GetString(AndroidJavaObject javaString)
         {
             return javaString.Call<string>("toString");
+        }
+
+        public static AndroidJavaObject GetList<T>(IList<T> list)
+        {
+            var javaList = new AndroidJavaObject("java.util.ArrayList");
+            foreach(T element in list)
+            {
+                javaList.Call("add", element);
+            }
+            return javaList;
+        }
+
+        public static IList<T> GetList<T>(AndroidJavaObject javaList)
+        {
+            var list = new List<T>();
+            int size = javaList.Call<int>("size");
+            for(int i = 0; i < size; i++)
+            {
+                var element = javaList.Call<T>("get", i);
+                list.Add(element);
+            }
+            return list;
         }
     }
 }
---
+++ HuaweiMobileServices/utils/ListUtils.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HuaweiMobileServices.Utils
+{
+    class ListUtils
+    {
+
+        private ListUtils() { }
+
+        public static IList<U> Map<T, U>(IList<T> list, Func<T, U> action)
+        {
+            var newList = new List<U>();
+            foreach(T element in list)
+            {
+                newList.Add(action.Invoke(element));
+            }
+            return newList;
+        }
+    }
+}
