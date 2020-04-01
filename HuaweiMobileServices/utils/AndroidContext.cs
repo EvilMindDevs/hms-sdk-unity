4f44fafae3a139d412d9c177c212bfcec5da8959
Yassine Diouri <ametric@gmail.com>
2020-04-01 15:32:58 +0200

    Added wrapper for Android Context

---
+++ HuaweiMobileServices/utils/AndroidContext.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace HuaweiMobileServices
+{
+    public class AndroidContext
+    {
+
+        private static AndroidJavaClass mUnityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+        private static AndroidJavaObject mApplicationContext = null;
+
+        private AndroidContext()
+        {
+        }
+
+        public static AndroidJavaObject getActivityContext()
+        {
+            return mUnityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+        }
+
+        public static AndroidJavaObject getApplicationContext()
+        {
+            if (mApplicationContext == null)
+            {
+                mApplicationContext = getActivityContext().Call<AndroidJavaObject>("getApplicationContext");
+            }
+            return mApplicationContext;
+        }
+    }
+
+}
