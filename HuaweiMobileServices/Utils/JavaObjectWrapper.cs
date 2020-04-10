56d22f1976b38260c96336498e7e379aef39e4b6
Yassine Diouri <ametric@gmail.com>
2020-04-10 23:55:29 +0200

    Added automatic parameters in JavaObjectWrapper

--- HuaweiMobileServices/Utils/JavaObjectWrapper.cs
+++ HuaweiMobileServices/Utils/JavaObjectWrapper.cs
@@ -16,11 +16,33 @@ namespace HuaweiMobileServices.Utils
             JavaObject = new AndroidJavaObject(javaObjectCanonicalName, args);
         }
 
+        private static object[] AsAutoParams(object[] args)
+        {
+            var newArgs = new object[args.Length];
+            for (int i = 0; i < args.Length; i++)
+            {
+                var arg = args[i];
+                if (arg is JavaObjectWrapper)
+                {
+                    newArgs[i] = (arg as JavaObjectWrapper).JavaObject;
+                }
+                else if (arg is string)
+                {
+                    newArgs[i] = (arg as string).AsJavaString();
+                }
+                else
+                {
+                    newArgs[i] = arg;
+                }
+            }
+            return newArgs;
+        }
+
         internal protected virtual AndroidJavaObject JavaObject { get; set; }
 
-        internal protected T Call<T>(string methodName, params object[] args) => JavaObject.Call<T>(methodName, args);
+        internal protected T Call<T>(string methodName, params object[] args) => JavaObject.Call<T>(methodName, AsAutoParams(args));
 
-        internal protected void Call(string methodName, params object[] args) => JavaObject.Call(methodName, args);
+        internal protected void Call(string methodName, params object[] args) => JavaObject.Call(methodName, AsAutoParams(args));
 
         internal protected string CallAsString(string methodName, params object[] args) =>
             Call<AndroidJavaObject>(methodName, args).AsString();
