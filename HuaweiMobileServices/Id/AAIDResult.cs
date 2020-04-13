10a3e810d5105a9d2058f49cf13ac0eefa0cf50b
Yassine Diouri <ametric@gmail.com>
2020-04-13 13:56:36 +0200

    Login for push

---
+++ HuaweiMobileServices/Id/AAIDResult.cs
@@ -0,0 +1,17 @@
+﻿namespace HuaweiMobileServices.Id
+{
+
+    using HuaweiMobileServices.Utils;
+    using UnityEngine;
+
+    // Wrapper for com.huawei.hms.aaid.entity.AAIDResult
+    public class AAIDResult : JavaObjectWrapper
+    {
+
+        public AAIDResult(AndroidJavaObject javaObject) : base(javaObject) { }
+
+        public virtual string Id => CallAsString("getId");
+
+    }
+
+}
---
+++ HuaweiMobileServices/Id/HmsInstanceId.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace HuaweiMobileServices.Id
+{
+
+    using UnityEngine;
+    using HuaweiMobileServices.Utils;
+    using HuaweiMobileServices.Base;
+
+    // Wrapper for com.huawei.hms.aaid.HmsInstanceId
+    public class HmsInstanceId : JavaObjectWrapper
+    {
+
+        private static readonly AndroidJavaClass sJavaClass = new AndroidJavaClass("com.huawei.hms.aaid.HmsInstanceId");
+
+        public HmsInstanceId(AndroidJavaObject javaObject) : base(javaObject) { }
+
+        public static HmsInstanceId GetInstance() =>
+            sJavaClass.CallStaticAsWrapper<HmsInstanceId>("getInstance", AndroidContext.GetApplicationContext());
+
+        public virtual string Id => CallAsString("getId");
+
+        public virtual ITask<AAIDResult> AAID => CallAsWrapper<TaskJavaObjectWrapper<AAIDResult>>("getAAID");
+
+        public virtual long CreationTime => Call<long>("getCreationTime");
+
+        public virtual void DeleteAAID() => Call("deleteAAID");
+
+        public virtual string GetToken(string paramString1, string paramString2) =>
+            CallAsString("getToken", paramString1, paramString2);
+
+        public virtual void DeleteToken(string paramString1, string paramString2) =>
+            Call("deleteToken", paramString1, paramString2);
+
+    }
+
+}
