17a024961008f25cfd8eb6e3c18ac32b6b196988
Yassine Diouri <ametric@gmail.com>
2020-04-14 17:10:14 +0200

    Initial Android native bridge project

---
+++ android-hms-wrapper/app/src/main/java/org/m0skit0/android/hms/unity/MainActivity.kt
@@ -0,0 +1,11 @@
+package org.m0skit0.android.hms.unity
+
+import android.app.Activity
+import android.os.Bundle
+
+class NativeBridgeActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
