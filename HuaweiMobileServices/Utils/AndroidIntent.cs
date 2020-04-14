39503b5148f55c6651925b976dfd3a9f3de2cba6
Yassine Diouri <ametric@gmail.com>
2020-04-14 13:18:53 +0200

    Extended AndroidIntent class functionality

--- HuaweiMobileServices/Utils/AndroidIntent.cs
+++ HuaweiMobileServices/Utils/AndroidIntent.cs
@@ -4,8 +4,19 @@
 
     public class AndroidIntent : JavaObjectWrapper
     {
+        private const string CLASS_NAME = "android.content.Intent";
+
         public AndroidIntent(AndroidJavaObject javaObject) : base(javaObject) { }
 
+        public AndroidIntent(AndroidJavaClass activityClass) : base(CLASS_NAME, AndroidContext.GetActivityContext(), activityClass) { }
+
         public AndroidJavaObject Intent => JavaObject;
+
+        public AndroidIntent PutExtra(string name, AndroidJavaObject value)
+        {
+            JavaObject = JavaObject.Call<AndroidJavaObject>("putExtra", value);
+            return this;
+        }
+
     }
 }
