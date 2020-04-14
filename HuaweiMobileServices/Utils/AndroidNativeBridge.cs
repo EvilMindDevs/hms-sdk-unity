9fb534a104f3b35236db4e95235ea4bb1af3c667
Yassine Diouri <ametric@gmail.com>
2020-04-14 13:52:01 +0200

    Bridging code

---
+++ HuaweiMobileServices/Utils/AndroidNativeBridge.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace HuaweiMobileServices.Utils
+{
+    using UnityEngine;
+    using HuaweiMobileServices.Base;
+
+    internal class AndroidNativeBridge<T>
+    {
+
+        private static readonly AndroidJavaClass NATIVE_BRIDGE_ACTIVITY = new AndroidJavaClass("com.pues.lo.que.sea.NativeBridgeActivity");
+
+        private const string STATUS_NAME = "status";
+
+        private AndroidIntent mIntent = new AndroidIntent(NATIVE_BRIDGE_ACTIVITY);
+
+        private Action<Exception> mFailureAction;
+        private Action<T> mSuccessAction;
+
+        public AndroidNativeBridge<T> AddOnFailureListener(Action<Exception> onFailureListener)
+        {
+            mFailureAction = onFailureListener;
+            return this;
+        }
+
+        public AndroidNativeBridge<T> AddOnSuccessListener(Action<T> onSuccessListener)
+        {
+            mSuccessAction = onSuccessListener;
+            return this;
+        }
+
+        public AndroidNativeBridge<T> AddJavaObject(string name, AndroidJavaObject javaObject)
+        {
+            mIntent.PutExtra(name, javaObject);
+            return this;
+        }
+
+        public void Execute()
+        {
+            AndroidContext.GetActivityContext().Call("startActivity", mIntent);
+        }
+    }
+}
