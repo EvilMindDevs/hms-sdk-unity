97d9d8be798cc726354ffa7747a092b628b26d91
Yassine Diouri <ametric@gmail.com>
2020-04-15 12:10:18 +0200

    Added simple callback for testing

---
+++ android-wrapper/app/src/main/java/org/m0skit0/android/hms/unity/BridgeCallback.kt
@@ -0,0 +1,5 @@
+package org.m0skit0.android.hms.unity
+
+interface BridgeCallback {
+    fun onCallback()
+}
--- android-wrapper/app/src/main/java/org/m0skit0/android/hms/unity/base/StatusBridge.kt
+++ android-wrapper/app/src/main/java/org/m0skit0/android/hms/unity/base/StatusBridge.kt
@@ -3,25 +3,22 @@ package org.m0skit0.android.hms.unity.base
 import android.content.Intent
 import com.huawei.hms.support.api.client.Status
 import com.unity3d.player.UnityPlayer
+import org.m0skit0.android.hms.unity.BridgeCallback
 import org.m0skit0.android.hms.unity.activity.NativeBridgeActivity
 import org.m0skit0.android.hms.unity.activity.TYPE
-
-data class StatusBridgeData(
-    internal val gameObject: String,
-    internal val gameObjectMethod: String,
-    internal val status: Status
-)
 
 object StatusBridge {
 
     internal const val CODE = 0
     internal const val STATUS = "STATUS"
 
-    private lateinit var statusBridgeData: StatusBridgeData
+    private lateinit var status: Status
+    private lateinit var callback: BridgeCallback
 
     @JvmStatic
-    fun receiveStartResolutionForResult(data: StatusBridgeData) {
-        statusBridgeData = data
+    fun receiveStartResolutionForResult(status: Status, callback: BridgeCallback) {
+        this.status = status
+        this.callback = callback
         Intent(UnityPlayer.currentActivity, NativeBridgeActivity::class.java)
             .putExtra(TYPE, STATUS)
             .let { intent ->
@@ -30,10 +27,10 @@ object StatusBridge {
     }
 
     fun launchStartResolutionForResult(activity: NativeBridgeActivity) {
-        statusBridgeData.status.startResolutionForResult(activity, CODE)
+        status.startResolutionForResult(activity, CODE)
     }
 
     fun returnStartResolutionForResult() {
-        UnityPlayer.UnitySendMessage(statusBridgeData.gameObject, statusBridgeData.gameObjectMethod, "")
+        callback.onCallback()
     }
 }
