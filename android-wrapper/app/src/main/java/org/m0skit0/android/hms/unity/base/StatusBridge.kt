1613bda4d9b007886f7a8c90edfbe1ad4efd8c8e
Yassine Diouri <ametric@gmail.com>
2020-04-15 11:19:02 +0200

    Renamed StatusWrapperData to StatusBridgeData

--- android-wrapper/app/src/main/java/org/m0skit0/android/hms/unity/base/StatusBridge.kt
+++ android-wrapper/app/src/main/java/org/m0skit0/android/hms/unity/base/StatusBridge.kt
@@ -1,13 +1,12 @@
 package org.m0skit0.android.hms.unity.base
 
-import android.app.Activity
 import android.content.Intent
 import com.huawei.hms.support.api.client.Status
 import com.unity3d.player.UnityPlayer
 import org.m0skit0.android.hms.unity.activity.NativeBridgeActivity
 import org.m0skit0.android.hms.unity.activity.TYPE
 
-data class StatusWrapperData(
+data class StatusBridgeData(
     internal val gameObject: String,
     internal val gameObjectMethod: String,
     internal val status: Status
@@ -18,11 +17,11 @@ object StatusBridge {
     internal const val CODE = 0
     internal const val STATUS = "STATUS"
 
-    private lateinit var statusWrapperData: StatusWrapperData
+    private lateinit var statusBridgeData: StatusBridgeData
 
     @JvmStatic
-    fun receiveStartResolutionForResult(data: StatusWrapperData) {
-        statusWrapperData = data
+    fun receiveStartResolutionForResult(data: StatusBridgeData) {
+        statusBridgeData = data
         Intent(UnityPlayer.currentActivity, NativeBridgeActivity::class.java)
             .putExtra(TYPE, STATUS)
             .let { intent ->
@@ -31,10 +30,10 @@ object StatusBridge {
     }
 
     fun launchStartResolutionForResult(activity: NativeBridgeActivity) {
-        statusWrapperData.status.startResolutionForResult(activity, CODE)
+        statusBridgeData.status.startResolutionForResult(activity, CODE)
     }
 
     fun returnStartResolutionForResult() {
-        UnityPlayer.UnitySendMessage(statusWrapperData.gameObject, statusWrapperData.gameObjectMethod, "")
+        UnityPlayer.UnitySendMessage(statusBridgeData.gameObject, statusBridgeData.gameObjectMethod, "")
     }
 }
