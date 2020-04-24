c65ddc6dfb0eb8071352a2f6ef0e6a690f7b56b0
Yassine Diouri <ametric@gmail.com>
2020-04-24 17:37:25 +0200

    Created structure and hierarchy for using IapApiException

--- hms-sdk-unity/HuaweiMobileServices/IAP/ProductInfo.cs
+++ hms-sdk-unity/HuaweiMobileServices/IAP/ProductInfo.cs
@@ -13,102 +13,85 @@
             get => Call<AndroidJavaObject>("getProductId").AsString();
         }
 
-
         public virtual int PriceType
         {
             get => Call<int>("getPriceType");
         }
-
 
         public virtual string Price
         {
             get => Call<AndroidJavaObject>("getPrice").AsString();
         }
 
-
         public virtual long MicrosPrice
         {
             get => Call<long>("getMicrosPrice");
         }
-
 
         public virtual string OriginalLocalPrice
         {
             get => Call<AndroidJavaObject>("getOriginalLocalPrice").AsString();
         }
 
-
         public virtual long OriginalMicroPrice
         {
             get => Call<long>("getOriginalMicroPrice");
         }
-
 
         public virtual string Currency
         {
             get => Call<AndroidJavaObject>("getCurrency").AsString();
         }
 
-
         public virtual string ProductName
         {
             get => Call<AndroidJavaObject>("getProductName").AsString();
         }
-
 
         public virtual string ProductDesc
         {
             get => Call<AndroidJavaObject>("getProductDesc").AsString();
         }
 
-
         public virtual string SubPeriod
         {
             get => Call<AndroidJavaObject>("getSubPeriod").AsString();
         }
-
 
         public virtual string SubSpecialPrice
         {
             get => Call<AndroidJavaObject>("getSubSpecialPrice").AsString();
         }
 
-
         public virtual long SubSpecialPriceMicros
         {
             get => Call<long>("getSubSpecialPriceMicros");
         }
-
 
         public virtual string SubSpecialPeriod
         {
             get => Call<AndroidJavaObject>("getSubSpecialPeriod").AsString();
         }
 
-
         public virtual int SubSpecialPeriodCycles
         {
             get => Call<int>("getSubSpecialPeriodCycles");
         }
-
 
         public virtual string SubFreeTrialPeriod
         {
             get => Call<AndroidJavaObject>("getSubFreeTrialPeriod").AsString();
         }
 
-
         public virtual string SubGroupId
         {
             get => Call<AndroidJavaObject>("getSubGroupId").AsString();
         }
 
-
         public virtual string SubGroupTitle
         {
             get => Call<AndroidJavaObject>("getSubGroupTitle").AsString();
         }
-
 
         public virtual int SubProductLevel
         {
