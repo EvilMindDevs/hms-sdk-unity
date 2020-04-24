608ac9e5a41a02fbd42a84bbefc5d5262c4428c7
Yassine Diouri <ametric@gmail.com>
2020-04-24 19:31:45 +0200

    Added InterstitialAd class

---
+++ hms-sdk-unity/HuaweiMobileServices/Ads/InterstitialAd.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HuaweiMobileServices.Ads
+{
+
+    using HuaweiMobileServices.Utils;
+    using UnityEngine;
+
+    // Wrapper for com.huawei.hms.ads.InterstitialAd
+    public class InterstitialAd : JavaObjectWrapper
+    {
+
+        public InterstitialAd() : base("com.huawei.hms.ads.InterstitialAd", AndroidContext.ActivityContext) { }
+        
+        public IAdListener AdListener
+        {
+            set => Call("setAdListener", new AdListenerWrapper(value));
+        }
+
+        public string AdId
+        {
+            set => Call("setAdId", value);
+        }
+
+        public bool Loaded => Call<bool>("isLoaded");
+
+        public bool Loading => Call<bool>("isLoading");
+
+        public void LoadAd(AdParam paramAdParam)
+        {
+            Call("loadAd", paramAdParam);
+        }
+
+        public void Show()
+        {
+            Call("show");
+        }
+
+        public IRewardAdListener RewardAdListener
+        {
+            set => Call("setRewardAdListener", new RewardAdListenerWrapper(value));
+        }
+        
+    }
+
+}
