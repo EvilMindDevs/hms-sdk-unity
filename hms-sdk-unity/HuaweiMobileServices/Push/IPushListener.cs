ccb38336f17dea8f68596da2d7b9573c2244677e
Yassine Diouri <ametric@gmail.com>
2020-04-25 12:19:14 +0200

    Add Push implementation for C# part

---
+++ hms-sdk-unity/HuaweiMobileServices/Push/IPushListener.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HuaweiMobileServices.Push
+{
+    public interface IPushListener
+    {
+        void OnNewToken(string token);
+        void OnTokenError(Exception e);
+        void OnMessageReceived(RemoteMessage remoteMessage);
+    }
+}
---
+++ hms-sdk-unity/HuaweiMobileServices/Push/PushListenerWrapper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HuaweiMobileServices.Push
+{
+    using UnityEngine;
+    using HuaweiMobileServices.Base;
+    using HuaweiMobileServices.Utils;
+
+    internal class PushListenerWrapper : AndroidJavaProxy
+    {
+
+        private readonly IPushListener mListener;
+
+        public PushListenerWrapper(IPushListener listener) : base("org.m0skit0.android.hms.unity.push.PushListener")
+        {
+            mListener = listener;
+        }
+
+        public void onNewToken(string token)
+        {
+            mListener?.OnNewToken(token);
+        }
+
+        public void onTokenError(AndroidJavaObject e)
+        {
+            mListener?.OnTokenError(e.AsHMSException());
+        }
+
+        public void onMessageReceived(AndroidJavaObject remoteMessage)
+        {
+            mListener?.OnMessageReceived(remoteMessage.AsWrapper<RemoteMessage>());
+        }
+    }
+}
