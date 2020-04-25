3c7d13bb21a2aa630ffd85a64ad77c65dc3fdf77
Yassine Diouri <ametric@gmail.com>
2020-04-25 15:40:49 +0200

    Add missing Game APIs

---
+++ hms-sdk-unity/HuaweiMobileServices/Utils/ParameterUtils.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HuaweiMobileServices.Utils
+{
+    internal static class ParameterUtils
+    {
+
+        public static object[] AsAutoParams(this object[] args)
+        {
+            var newArgs = new object[args.Length];
+            for (int i = 0; i < args.Length; i++)
+            {
+                newArgs[i] = AsAutoType(args[i]);
+            }
+            return newArgs;
+        }
+
+        public static object AsAutoType(this object arg)
+        {
+            if (arg is JavaObjectWrapper) return (arg as JavaObjectWrapper).JavaObject;
+            if (arg is string) return (arg as string).AsJavaString();
+            return arg;
+        }
+
+    }
+}
