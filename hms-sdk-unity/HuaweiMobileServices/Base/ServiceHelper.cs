2a2d51cae4b32a43d95498f0bf3e3faeaca1ec51
Yassine Diouri <ametric@gmail.com>
2020-05-04 17:12:18 +0200

    Added ServiceHelper wrapper in C#

---
+++ hms-sdk-unity/HuaweiMobileServices/Base/ServiceHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HuaweiMobileServices.Base
+{
+    using UnityEngine;
+
+    public static class ServiceHelper
+    {
+        private static readonly AndroidJavaClass sJavaClass = new AndroidJavaClass("org.m0skit0.android.hms.unity.helper.ServiceHelper");
+
+        public static bool GMSAvailable => sJavaClass.Call<bool>("isGMSAvailable");
+
+        public static bool HMSAvailable => sJavaClass.Call<bool>("isHMSAvailable");
+    }
+}
