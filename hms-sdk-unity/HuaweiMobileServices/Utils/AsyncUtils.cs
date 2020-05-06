e57fc8fd1c041b1ae5f4e89afd6148e61a60045e
Yassine Diouri <ametric@gmail.com>
2020-05-06 18:00:05 +0200

    Added ITask.AsAsync() extension method

---
+++ hms-sdk-unity/HuaweiMobileServices/Utils/AsyncUtils.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+
+namespace HuaweiMobileServices.Utils
+{
+    using HuaweiMobileServices.Base;
+    using HuaweiMobileServices.Id;
+
+    public static class AsyncUtils
+    {
+        public static Task<T> AsAsync<T>(this ITask<T> iTask)
+        {
+            var task = new TaskCompletionSource<T>();
+            iTask
+                .AddOnSuccessListener((result) => task.SetResult(result))
+                .AddOnFailureListener((error) => task.SetException(error));
+            return task.Task;
+        }
+    }
+}
