a2e0d927cf508030b0b7e96237d5cf690cf19cb5
y00429754 <yusufaltungoldtr@hotmail.com>
2020-08-20 00:19:32 +0300

    Banner ads add with size,position,hide,delete option

---
+++ hms-sdk-unity/HuaweiMobileServices/Ads/BannerAd.cs
@@ -0,0 +1,90 @@
+﻿using HuaweiMobileServices.Utils;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace HuaweiMobileServices.Ads
+{
+    public class BannerAd : JavaObjectWrapper
+    {
+
+        [UnityEngine.Scripting.Preserve]
+        public BannerAd(AndroidJavaObject javaObject) : base(javaObject) { }
+       
+        public BannerAd(AdStatusListener mAdStatusListener) : base("org.m0skit0.android.hms.unity.ads.BannerAdProxy", AndroidContext.ActivityContext, mAdStatusListener) { }
+         
+        public string AdId
+        {
+            set;
+            get;
+        }
+
+        public int positionType
+        {
+            set;
+            get;
+        }
+        public String sizeType
+        {
+            set;
+            get;
+        }
+        public AdStatusListener mAdStatusListener
+        {
+            set;
+            get;
+        }
+        public void showBanner(AdParam adRequest) {
+            handleRequestAd(adRequest);  
+        }
+        public void hideBanner()
+        {
+             handleHideAd();
+        }
+        public void destroyBanner( )
+        {
+            handleDestroyAd();
+        }
+        private void handleRequestAd(AdParam adRequest)
+        {
+            mAdStatusListener.mOnAdLoaded += onAdLoadSuccess;
+            mAdStatusListener.mOnAdFailed += onAdLoadFail; 
+             
+            Call("setAdId", AdId);
+
+            Call("setBannerAdPosition", positionType);
+            
+            Call("setAdSizeType", sizeType);
+
+            Call("loadAd", adRequest);    
+        }
+
+        private void onAdLoadFail(object sender, AdLoadErrorCodeEventArgs args)
+        {
+            Debug.Log("[HMS] Bannerads onAdLoadFail" + args);
+        }
+        private void onAdLoadSuccess(object sender, EventArgs args)
+        {
+            Debug.Log("[HMS] Bannerads onAdLoadSuccess");
+            Call("show");
+        }
+
+        private void handleHideAd()
+        {
+            if (this != null)
+            {
+                 Call("hide");
+            }
+        }
+
+        private void handleDestroyAd()
+        {
+            if (this != null)
+            {
+                 Call("destroy");
+            }
+        }
+
+    }
+}
