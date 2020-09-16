9472818a3726db6a1123169af2f903753bca53ff
y00429754 <yusufaltungoldtr@hotmail.com>
2020-09-16 10:05:42 +0300

    Added Analytics wrapper

---
+++ hms-sdk-unity/HuaweiMobileServices/Analystics/HiAnalyticsInstance.cs
@@ -0,0 +1,90 @@
+﻿using HuaweiMobileServices.Base;
+using HuaweiMobileServices.Utils;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace HuaweiMobileServices.Analystics
+{
+
+    public sealed class HiAnalyticsInstance : JavaObjectWrapper
+    {
+
+        [UnityEngine.Scripting.Preserve]
+        public HiAnalyticsInstance(AndroidJavaObject hiAnalyticsInstance) : base(hiAnalyticsInstance) {}
+
+        public void setAnalyticsEnabled(bool enabled)
+        {
+            Call("setAnalyticsEnabled", enabled);
+        }
+        public void setAutoCollectionEnabled(bool isEnabled)
+        {
+            Call("setAutoCollectionEnabled", isEnabled);
+        }
+
+        public void setUserId(String id)
+        {
+            Call("setUserId", id);
+        }
+        public void setUserProfile(String name, String value)
+        {
+            Call("setUserProfile", name, value);
+        }
+        public void setPushToken(String token)
+        {
+            Call("setPushToken", token);
+        }
+        public void setMinActivitySessions(long milliseconds)
+        {
+            Call("setMinActivitySessions", milliseconds);
+        }
+        public void setSessionDuration(long milliseconds)
+        {
+            Call("setSessionDuration", milliseconds);
+        }
+        public void setCurrentActivity(String activityName, String activityClassOverride)
+        {
+            //AndroidContext activity, 
+            Call("setCurrentActivity", activityName, activityClassOverride);
+        }
+        public void onEvent(String eventId, Bundle androidBundle)
+        {
+            Call("onEvent", eventId, androidBundle);
+        }
+        public void clearCachedData()
+        {
+            Call("clearCachedData");
+        }
+        public Task getAAID()
+        {
+            return Call<Task>("getAAID"); 
+        }
+ 
+        public void regHmsSvcEvent()
+        {
+            Call("regHmsSvcEvent");
+        }
+        public void unRegHmsSvcEvent()
+        {
+            Call("unRegHmsSvcEvent");
+        }
+        public Map<String, String> getUserProfiles(bool var1)
+        {
+            return Call<Map<String, String>>("getUserProfiles", var1); 
+        }
+        public void pageStart(String pageName, String pageClassOverride)
+        {
+            Call("pageStart", pageName, pageClassOverride);
+        }
+        public void pageEnd(String pageName)
+        {
+            Call("pageEnd", pageName);
+        }
+
+
+
+    }
+
+}
+
---
+++ hms-sdk-unity/HuaweiMobileServices/Analystics/HiAnalyticsTools.cs
@@ -0,0 +1,24 @@
+﻿ 
+using UnityEngine;
+
+namespace HuaweiMobileServices.Analystics
+{
+    public static class HiAnalyticsTools
+    {
+        private static AndroidJavaClass sIapClass = new AndroidJavaClass("com.huawei.hms.analytics.HiAnalyticsTools");
+
+        public static void enableLog()
+        {
+            sIapClass.CallStatic("enableLog");
+        }
+        /**
+         * Log.DEBUG(3) Log.INFO(4) Log.WARN(5)  Log.ERROR(6)
+         **/
+        public static void enableLog(int level)
+        {
+            sIapClass.CallStatic("enableLog", level);
+        }
+
+        
+    }
+}
