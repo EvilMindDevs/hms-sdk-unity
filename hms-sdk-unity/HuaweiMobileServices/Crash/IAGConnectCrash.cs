bcdff0918f8f7f4a50ef775f0fc575b073b4f0cf
Kayahan Baskeser <kayahan.baskeser@gmail.com>
2020-11-13 14:27:35 +0300

    Added Crash Service

---
+++ hms-sdk-unity/HuaweiMobileServices/Crash/IAGConnectCrash.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace HuaweiMobileServices.Crash
+{
+    public interface IAGConnectCrash
+    {
+        void EnableCrashCollection(Boolean enable);
+        void TestIt();
+        void SetUserId(String userId);
+        void SetCustomKey(String key, String value);
+        void SetCustomKey(String key, Boolean value);
+        void SetCustomKey(String key, double value);
+        void SetCustomKey(String key, float value);
+        void SetCustomKey(String key, int value);
+        void SetCustomKey(String key, long value);
+        void Log(String message);
+        void Log(int level, String message);
+    }
+}
