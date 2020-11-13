91c8c5e5606922bea6df44730d62bf5c1fd8f6e2
Kayahan Baskeser <kayahan.baskeser@gmail.com>
2020-11-13 14:32:53 +0300

    Fixed ServiceHelper Class

--- hms-sdk-unity/HuaweiMobileServices/Base/ServiceHelper.cs
+++ hms-sdk-unity/HuaweiMobileServices/Base/ServiceHelper.cs
@@ -6,8 +6,8 @@
     {
         private static readonly AndroidJavaClass sJavaClass = new AndroidJavaClass("org.m0skit0.android.hms.unity.helper.ServiceHelper");
 
-        public static bool GMSAvailable => sJavaClass.Call<bool>("isGMSAvailable");
+        public static bool GMSAvailable => sJavaClass.CallStatic<bool>("isGMSAvailable");
 
-        public static bool HMSAvailable => sJavaClass.Call<bool>("isHMSAvailable");
+        public static bool HMSAvailable => sJavaClass.CallStatic<bool>("isHMSAvailable");
     }
 }
