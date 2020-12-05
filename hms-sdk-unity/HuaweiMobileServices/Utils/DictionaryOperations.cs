9737082dbcb67977e2c9c7c0a66747f030838669
Kayahan Baskeser <kayahan.baskeser@gmail.com>
2020-12-06 01:02:04 +0300

    Update AsJavaHasMap method

--- hms-sdk-unity/HuaweiMobileServices/Utils/DictionaryOperations.cs
+++ hms-sdk-unity/HuaweiMobileServices/Utils/DictionaryOperations.cs
@@ -12,27 +12,55 @@ namespace HuaweiMobileServices.Utils
         internal static AndroidJavaObject AsJavaHashMap(this IDictionary<string, object> dictionary)
         {
             AndroidJavaObject javaMap = new AndroidJavaObject("java.util.HashMap");
-            IntPtr putMethod = AndroidJNIHelper.GetMethodID(
-                javaMap.GetRawClass(), "put",
-                    "(Ljava/lang/Object;Ljava/lang/Object;)Ljava/lang/Object;");
 
-            object[] args = new object[2];
-            foreach (KeyValuePair<String, System.Object> kvp in dictionary)
+            IntPtr putMethod = AndroidJNIHelper.GetMethodID(javaMap.GetRawClass(), "put", "(Ljava/lang/Object;Ljava/lang/Object;)Ljava/lang/Object;");
+
+            foreach (KeyValuePair<string, object> kvp in dictionary)
             {
-                using (AndroidJavaObject k = new AndroidJavaObject(
-                    "java.lang.String", kvp.Key))
+                using (AndroidJavaObject k = new AndroidJavaObject("java.lang.String", kvp.Key))
                 {
-                    using (AndroidJavaObject v = new AndroidJavaObject(
-                        "java.lang.String", kvp.Value))
+                    switch (kvp.Value.GetType().ToString())
                     {
-                        args[0] = k;
-                        args[1] = v;
-                        AndroidJNI.CallObjectMethod(javaMap.GetRawObject(),
-                                putMethod, AndroidJNIHelper.CreateJNIArgArray(args));
+                        case "System.String":
+                            mapPut(k, kvp, putMethod, javaMap, "java.lang.String");
+                            break;
+                        case "System.Boolean":
+                            mapPut(k, kvp, putMethod, javaMap, "java.lang.Boolean");
+                            break;
+                        case "System.Int32":
+                            mapPut(k, kvp, putMethod, javaMap, "java.lang.Integer");
+                            break;
+                        case "System.Double":
+                            mapPut(k, kvp, putMethod, javaMap, "java.lang.Double");
+                            break;
+                        case "System.Byte[]":
+                            mapPut(k, kvp, putMethod, javaMap, "java.lang.Arrays");
+                            break;
+                        default:
+                            Console.WriteLine("RemoteConfig: Unknown type");
+                            break;
                     }
                 }
             }
             return javaMap;
+        }
+
+        private static void mapPut(AndroidJavaObject k, KeyValuePair<string, object> kvp, IntPtr putMethod, AndroidJavaObject javaMap, string obje)
+        {
+            try
+            {
+                object[] args = new object[2];
+                using (AndroidJavaObject v = new AndroidJavaObject(obje, kvp.Value))
+                {
+                    args[0] = k;
+                    args[1] = v;
+                    AndroidJNI.CallObjectMethod(javaMap.GetRawObject(),putMethod, AndroidJNIHelper.CreateJNIArgArray(args));
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.Log($"RemoteConfig, AsJavaHashMap Error {e.Message}");
+            }
         }
 
         internal static Dictionary<string, object> XMLtoDictionary(String xmlPath)
