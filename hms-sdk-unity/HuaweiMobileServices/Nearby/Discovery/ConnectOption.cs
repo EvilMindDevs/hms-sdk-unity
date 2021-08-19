b78eb60ba4812e27250e9852cd95ef0b3910b4d5
yusuf altun <yusufaltungoldtr@hotmail.com>
2021-08-19 11:57:06 +0300

    connectoption added, bug fix

---
+++ hms-sdk-unity/HuaweiMobileServices/Nearby/Discovery/ConnectOption.cs
@@ -0,0 +1,15 @@
+﻿using HuaweiMobileServices.Utils;
+using UnityEngine;
+
+namespace HuaweiMobileServices.Nearby.Discovery
+{
+    public class ConnectOption : JavaObjectWrapper
+    {
+        public ConnectOption(AndroidJavaObject javaObject) : base(javaObject) { }
+
+        private static AndroidJavaClass androidJavaClass = new AndroidJavaClass("com.huawei.hms.nearby.discovery.ChannelPolicy");
+        public static ConnectOption CHANNEL_AUTO => androidJavaClass.GetStaticAsWrapper<ConnectOption>("CHANNEL_AUTO");
+        public static ConnectOption CHANNEL_HIGH_THROUGHPUT => androidJavaClass.GetStaticAsWrapper<ConnectOption>("CHANNEL_HIGH_THROUGHPUT");
+        public static ConnectOption CHANNEL_INSTANCE => androidJavaClass.GetStaticAsWrapper<ConnectOption>("CHANNEL_INSTANCE");
+    }
+}
