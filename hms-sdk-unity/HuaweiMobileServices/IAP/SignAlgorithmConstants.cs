9a0b8004866f716235b027d71c92d20a4e695572
Mustafa CAN <mcqn14@gmail.com>
2022-01-31 15:13:26 +0300

    HUP-106 done Class name, file name and private static readonly AndroidJavaClass sJavaClass = new AndroidJavaClass("com.huawei.hms.iap.entity.SignAlgorithmConstants"); are changed due to renaming.

---
+++ hms-sdk-unity/HuaweiMobileServices/IAP/SignAlgorithmConstants.cs
@@ -0,0 +1,11 @@
+﻿namespace HuaweiMobileServices.IAP
+{
+    using UnityEngine;
+
+    public static class SignAlgorithmConstants
+    {
+        private static readonly AndroidJavaClass sJavaClass = new AndroidJavaClass("com.huawei.hms.iap.entity.SignAlgorithmConstants");
+
+        public static string SIGNATURE_ALGORITHM_SHA256WITHRSA_PSS = sJavaClass.GetStatic<string>("SIGNATURE_ALGORITHM_SHA256WITHRSA_PSS");
+    }
+}
