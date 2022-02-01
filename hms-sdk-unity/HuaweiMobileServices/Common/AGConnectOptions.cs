e7b31608701f85b95625fd658c1140bd07b5eb22
alihan98ersoy <alihan98ersoy@hotmail.com>
2022-02-01 21:33:33 +0300

    HUP-115 Cloud DB Update - implemented 1.5.0.300 Changes
    Added Common classes

---
+++ hms-sdk-unity/HuaweiMobileServices/Common/AGConnectOptions.cs
@@ -0,0 +1,18 @@
+﻿using HuaweiMobileServices.Utils;
+using UnityEngine;
+
+namespace HuaweiMobileServices.Common
+{
+    public class AGConnectOptions : JavaObjectWrapper
+    {
+        private static AndroidJavaClass javaClass = new AndroidJavaClass("com.huawei.agconnect.AGConnectOptions");
+
+        public AGConnectOptions(AndroidJavaObject javaObject) : base(javaObject) { }
+
+        public static AGCRoutePolicy GetRoutePolicy() => javaClass.Call<AGCRoutePolicy>("getRoutePolicy");
+        public static string GetPackageName() => javaClass.Call<string>("getPackageName");
+        public static string GetString(string path) => javaClass.Call<string>("getString",path.AsJavaString());
+        public static string GetString(string path, string def) => javaClass.Call<string>("getString", path.AsJavaString(), def.AsJavaString());
+
+    }
+}
---
+++ hms-sdk-unity/HuaweiMobileServices/Common/AGConnectOptionsBuilder.cs
@@ -0,0 +1,26 @@
+﻿using HuaweiMobileServices.Base;
+using HuaweiMobileServices.Utils;
+using UnityEngine;
+
+namespace HuaweiMobileServices.Common
+{
+    public class AGConnectOptionsBuilder : JavaObjectWrapper
+    {
+        private static AndroidJavaClass javaClass = new AndroidJavaClass("com.huawei.agconnect.AGConnectOptionsBuilder");
+
+        public AGConnectOptionsBuilder(AndroidJavaObject javaObject) : base(javaObject) { }
+
+        public static AGConnectOptionsBuilder SetProductId(string productId) => javaClass.CallStaticAsWrapper<AGConnectOptionsBuilder>("setProductId", productId.AsJavaString());
+        public static AGConnectOptionsBuilder SetAppId(string appId) => javaClass.CallStaticAsWrapper<AGConnectOptionsBuilder>("setAppId", appId.AsJavaString());
+        public static AGConnectOptionsBuilder SetCPId(string cpId) => javaClass.CallStaticAsWrapper<AGConnectOptionsBuilder>("setCPId", cpId.AsJavaString());
+        public static AGConnectOptionsBuilder SetClientSecret(string clientSecret) => javaClass.CallStaticAsWrapper<AGConnectOptionsBuilder>("setClientSecret", clientSecret.AsJavaString());
+        public static AGConnectOptionsBuilder SetClientId(string clientId) => javaClass.CallStaticAsWrapper<AGConnectOptionsBuilder>("setClientId", clientId.AsJavaString());
+        public static AGConnectOptionsBuilder SetApiKey(string apiKey) => javaClass.CallStaticAsWrapper<AGConnectOptionsBuilder>("setApiKey", apiKey.AsJavaString());
+        public static AGConnectOptionsBuilder SetCustomValue(string key, string value) => javaClass.CallStaticAsWrapper<AGConnectOptionsBuilder>("setCustomValue", key.AsJavaString(), value.AsJavaString());
+        public static AGConnectOptionsBuilder SetRoutePolicy(AGCRoutePolicy routePolicy) => javaClass.CallStaticAsWrapper<AGConnectOptionsBuilder>("setRoutePolicy", routePolicy);
+        public static AGConnectOptionsBuilder SetPackageName(string packageName) => javaClass.CallStaticAsWrapper<AGConnectOptionsBuilder>("setPackageName", packageName.AsJavaString());
+        public static AGConnectOptionsBuilder SetInputStream(AndroidJavaObject inputStream) => javaClass.CallStaticAsWrapper<AGConnectOptionsBuilder>("setInputStream", inputStream);
+        public static AGConnectOptions Build() => javaClass.CallStaticAsWrapper<AGConnectOptions>("build", AndroidContext.ActivityContext);
+
+    }
+}
