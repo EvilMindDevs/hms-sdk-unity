84f3493c604b3c9ebe30ea83473f160b9ff7e328
alihan98ersoy <alihan98ersoy@hotmail.com>
2022-02-01 21:33:33 +0300

    HUP-115 Cloud DB Update - implemented 1.5.0.300 Changes
    Added Common classes

---
+++ hms-sdk-unity/HuaweiMobileServices/Common/AGConnectInstance .cs
@@ -0,0 +1,19 @@
+﻿using HuaweiMobileServices.Base;
+using HuaweiMobileServices.Utils;
+using UnityEngine;
+
+namespace HuaweiMobileServices.Common
+{
+    public class AGConnectInstance : JavaObjectWrapper
+    {
+        private static AndroidJavaClass javaClass = new AndroidJavaClass("com.huawei.agconnect.AGConnectInstance");
+
+        public AGConnectInstance(AndroidJavaObject javaObject) : base(javaObject) { }
+        public static void Initialize() => javaClass.CallStatic("initialize", AndroidContext.ActivityContext);
+        public static void Initialize(AGConnectOptionsBuilder optionsBuilder) => javaClass.CallStatic("initialize", AndroidContext.ActivityContext, optionsBuilder.JavaObject);
+        public static AGConnectInstance BuildInstance(AGConnectOptionsBuilder optionsBuilder) => javaClass.CallStaticAsWrapper<AGConnectInstance>("buildInstance", optionsBuilder.JavaObject);
+        public static AGConnectInstance GetInstance() => javaClass.CallStaticAsWrapper<AGConnectInstance>("getInstance");
+        public static AGConnectOptions GetOptions() => javaClass.CallStaticAsWrapper<AGConnectOptions>("getOptions");
+
+    }
+}
