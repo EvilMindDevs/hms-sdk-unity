779b1c527e68fa8af7e97c2b3a42fc4087d1c4c6
alihan98ersoy <alihan98ersoy@hotmail.com>
2022-05-15 10:47:32 +0300

    com.huawei.hms:ads-lite:13.4.52.300 HwAds Updated
    getter and setter for AppActivateStyle&AppInstalledNotify,
    ActivateStyle Constant-class added

--- hms-sdk-unity/HuaweiMobileServices/Ads/HwAds.cs
+++ hms-sdk-unity/HuaweiMobileServices/Ads/HwAds.cs
@@ -6,7 +6,6 @@
     // Wrapper for com.huawei.hms.ads.HwAds
     public static class HwAds
     {
-
         private static readonly AndroidJavaClass sJavaClass = new AndroidJavaClass("com.huawei.hms.ads.HwAds");
 
         public static RequestOptions RequestOptions
@@ -22,5 +21,24 @@
         public static string SDKVersion { get => sJavaClass.CallStatic<string>("getSDKVersion"); }
 
         public static void SetConsent(string param1String) => sJavaClass.CallStatic("setConsent", param1String);
+
+        public static int AppActivateStyle
+        {
+            get => sJavaClass.GetStatic<int>("getAppActivateStyle");   // get method
+            set => sJavaClass.CallStatic("setAppActivateStyle", value);  // set method
+        }
+
+        public static bool AppInstalledNotify
+        {
+            get => sJavaClass.GetStatic<bool>("isAppInstalledNotify");   // get method
+            set => sJavaClass.CallStatic("setAppInstalledNotify", value);  // set method
+        }
+    }
+
+    // Wrapper for Constant-class => com.huawei.hms.ads.ActivateStyle
+    public class ActivateStyle
+    {
+        public static int BOTTOM_BANNER = 1;
+        public static int CONFIRM_DIALOG = 2;
     }
 }
