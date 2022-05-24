5629bf14b19698294841cb2a0ffa33e24160f58b
Enes Özdemir <enesozdemir6167@hotmail.com>
2022-05-24 14:43:56 +0300

    AppLinking and ShortAppLinking class added

---
+++ hms-sdk-unity/HuaweiMobileServices/AppLinking/AppLinking.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using HuaweiMobileServices.Utils;
+using UnityEngine;
+
+namespace HuaweiMobileServices.AppLinking
+{
+    public class AppLinking : JavaObjectWrapper
+    {
+        public AppLinking(AndroidJavaObject javaObject) : base(javaObject) { }
+        private static readonly AndroidJavaClass androidJavaClass = new AndroidJavaClass("com.huawei.agconnect.applinking.AppLinking");
+
+
+        public string GetUri => CallAsUriString("getUri");
+        //public string GetResourceUri() => Call<string>("getResourceUri");
+
+        // Wrapper for com.huawei.agconnect.applinking.AppLinking$Builder
+        public class Builder : JavaObjectWrapper
+        {
+
+            public Builder(AndroidJavaObject javaObject) : base(javaObject) { }
+
+            public Builder() : base("com.huawei.agconnect.applinking.AppLinking$Builder") { }
+
+            public AppLinking BuildAppLinking() => CallAsWrapper<AppLinking>("buildAppLinking");
+
+        }
+
+    }
+
+}
---
+++ hms-sdk-unity/HuaweiMobileServices/AppLinking/ShortAppLinking.cs
@@ -0,0 +1,21 @@
+﻿using HuaweiMobileServices.Utils;
+using UnityEngine;
+
+namespace HuaweiMobileServices.AppLinking
+{
+    class ShortAppLinking : JavaObjectWrapper
+    {
+        public ShortAppLinking(AndroidJavaObject javaObject) : base(javaObject) { }
+        private static readonly AndroidJavaClass androidJavaClass = new AndroidJavaClass("com.huawei.agconnect.applinking.ShortAppLinking");
+
+        public string GetShortUrl => CallAsUriString("getShortUrl");
+        public string GetTestUrl => CallAsUriString("getTestUrl");
+
+        public enum LENGTH
+        {
+            LONG,
+            SHORT
+        }
+
+    }
+}
