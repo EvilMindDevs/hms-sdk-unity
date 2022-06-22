b493f4528966ce0d53dd9bbc8caea9d2abb02ce4
Enes Özdemir <enesozdemir6167@hotmail.com>
2022-06-22 12:38:58 +0300

    Initial Commit

---
+++ hms-sdk-unity/HuaweiMobileServices/Location/ActivityConversionData.cs
@@ -0,0 +1,25 @@
+﻿using HuaweiMobileServices.Base;
+using HuaweiMobileServices.Utils;
+using UnityEngine;
+
+namespace HuaweiMobileServices.Location
+{
+    public class ActivityConversionData : JavaObjectWrapper
+    {
+        public ActivityConversionData(AndroidJavaObject javaObject) : base(javaObject) { }
+        private static readonly AndroidJavaClass javaClass = new AndroidJavaClass("com.huawei.hms.location.ActivityConversionData");
+
+
+        public ActivityConversionData() : base("com.huawei.hms.location.ActivityConversionData") { }
+
+        public ActivityConversionData(int activityType, int conversionType, long elapsedTimeFromReboot) : base("com.huawei.hms.location.ActivityConversionData") { }
+
+        //TODO creator public field CREATOR
+
+        public int GetActivityType => Call<int>("getActivityType");
+        public int GetConversionType => Call<int>("getConversionType");
+        public long GetElapsedTimeFromReboot => Call<long>("getElapsedTimeFromReboot");
+
+
+    }
+}
---
+++ hms-sdk-unity/HuaweiMobileServices/Location/ActivityConversionInfo.cs
@@ -0,0 +1,23 @@
+﻿using HuaweiMobileServices.Utils;
+using UnityEngine;
+
+namespace HuaweiMobileServices.Location
+{
+    class ActivityConversionInfo : JavaObjectWrapper
+    {
+        public ActivityConversionInfo(AndroidJavaObject javaObject) : base(javaObject) { }
+        private static readonly AndroidJavaClass javaClass = new AndroidJavaClass("com.huawei.hms.location.ActivityConversionInfo");
+
+        public static int ENTER_ACTIVITY_CONVERSION = 0;
+        public static int EXIT_ACTIVITY_CONVERSION = 1;
+
+        public ActivityConversionInfo() : base("com.huawei.hms.location.ActivityConversionInfo") { }
+
+        public ActivityConversionInfo(int activityType, int conversionTypet) : base("com.huawei.hms.location.ActivityConversionInfo") { }
+
+        public int getActivityType => Call<int>("getActivityType");
+        public int GetConversionType => Call<int>("getConversionType");
+
+
+    }
+}
