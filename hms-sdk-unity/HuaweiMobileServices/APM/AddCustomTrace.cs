a2f55f8686859c2808282267ec51d02246edd037
BNY\eymen <bunyamineymenalagoz@gmail.com>
2022-09-21 14:52:57 +0300

    HUP-170: APM

---
+++ hms-sdk-unity/HuaweiMobileServices/APM/AddCustomTrace.cs
@@ -0,0 +1,14 @@
+﻿using HuaweiMobileServices.Utils;
+
+using UnityEngine;
+
+namespace HuaweiMobileServices.APM
+{
+    public class AddCustomTrace : JavaObjectWrapper
+    {
+        public AddCustomTrace(AndroidJavaObject javaObject) : base(javaObject) { }
+        public AddCustomTrace() : base("com.huawei.agconnect.apms.instrument.AddCustomTrace") { }
+        public string Name() => CallAsString("name");
+        public bool Enabled() => Call<bool>("enabled");
+    }
+}
---
+++ hms-sdk-unity/HuaweiMobileServices/APM/NetworkMeasure.cs
@@ -0,0 +1,26 @@
+﻿using HuaweiMobileServices.Utils;
+
+using System.Collections.Generic;
+
+using UnityEngine;
+
+namespace HuaweiMobileServices.APM
+{
+    public class NetworkMeasure : JavaObjectWrapper
+    {
+        public NetworkMeasure(AndroidJavaObject javaObject) : base(javaObject) { }
+        public NetworkMeasure() : base("com.huawei.agconnect.apms.custom.NetworkMeasure") { }
+
+        public static readonly AndroidJavaClass networkMeasure = new AndroidJavaClass("com.huawei.agconnect.apms.custom.NetworkMeasure");
+        public void Start() => Call("start");
+        public void Stop() => Call("stop");
+        public void SetStatusCode(int statusCode) => Call("setStatusCode", statusCode);
+        public void SetBytesSent(long btyesSent) => Call("setBytesSent", btyesSent);
+        public void SetBytesReceived(long bytesReceived) => Call("setBytesReceived", bytesReceived);
+        public void SetContentType(string contentType) => Call("setContentType", contentType);
+        public void PutProperty(string propertyName, string propertyValue) => Call("putProperty", propertyName, propertyValue);
+        public void RemoveProperty(string propertyName) => Call("removeProperty", propertyName);
+        public Dictionary<string, string> GetProperties() => Call<Dictionary<string, string>>("getProperties");
+        public string GetProperty(string propertyName) => CallAsString("getProperty", propertyName);
+    }
+}
