0c2b5ad0947c8f8a2e0a716f5b7d2ca082cebfc7
alihan98ersoy <alihan98ersoy@hotmail.com>
2023-02-05 18:15:28 +0300

    HUP-540
    Added Alipay account support.
    Fixed minor issue with Weibo account support.

---
+++ hms-sdk-unity/HuaweiMobileServices/AuthService/AlipayAuthProvider.cs
@@ -0,0 +1,19 @@
+﻿using HuaweiMobileServices.Utils;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace HuaweiMobileServices.AuthService
+{
+    public class AlipayAuthProvider
+    {
+        private static AndroidJavaClass javaClass = new AndroidJavaClass("com.huawei.agconnect.auth.AlipayAuthProvider");
+
+        public static AGConnectAuthCredential CredentialWithAuthCode(string authCode)
+            => javaClass.CallStaticAsWrapper<AGConnectAuthCredential>("credentialWithAuthCode", authCode);
+
+        public static AGConnectAuthCredential CredentialWithAuthCode(string authCode, bool autoCreateUser)
+            => javaClass.CallStaticAsWrapper<AGConnectAuthCredential>("credentialWithAuthCode", authCode, autoCreateUser);
+    }
+}
