23f34b17d5020548b5341ca22ec0de5e76198d42
alihan98ersoy <alihan98ersoy@hotmail.com>
2023-02-06 14:39:19 +0300

    HUP-534
    Added the AdvertiserInfo API to obtain and display advertiser information, adapting the Russian advertising law.
    Added the hasAdvertiserInfo and getAdvertiserInfo methods to the NativeAd class.
    
    Added the getInstallChannel method to the ReferrerDetails class to support the function of obtaining the channel information.

---
+++ hms-sdk-unity/HuaweiMobileServices/Ads/AdvertiserInfo.cs
@@ -0,0 +1,30 @@
+﻿using HuaweiMobileServices.Utils;
+using UnityEngine;
+
+namespace HuaweiMobileServices.Ads
+{
+    public class AdvertiserInfo : JavaObjectWrapper
+    {
+        //wrapper for com.huawei.hms.ads.AdSize
+        //https://developer.huawei.com/consumer/en/doc/development/HMSCore-References/adsize-0000001050064882
+
+        public AdvertiserInfo(AndroidJavaObject javaObject) : base(javaObject) { }
+
+        private static AndroidJavaClass sJavaClass = new AndroidJavaClass("com.huawei.hms.ads.AdvertiserInfo");
+
+        public int Sequence
+        {
+            get => Call<AndroidJavaObject>("getSeq").AsInt();
+        }
+
+        public string Key
+        {
+            get => Call<string>("getKey");
+        }
+
+        public string Value
+        {
+            get => Call<string>("getValue");
+        }
+    }
+}
