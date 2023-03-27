25bc00ab5931fcc013b164608a86c43d4581c922
Seyyid Yiğit <seyyid364@gmail.com>
2023-03-27 14:25:34 +0300

    Extra methods and arguments were removed. Asjavastring extension was used for string. Fixed getInstance metod call style.

--- hms-sdk-unity/HuaweiMobileServices/Modeling3D/ObjReconstructSdk/ReconstructApplication.cs
+++ hms-sdk-unity/HuaweiMobileServices/Modeling3D/ObjReconstructSdk/ReconstructApplication.cs
@@ -5,21 +5,20 @@ namespace HuaweiMobileServices.Modeling3D.ObjReconstructSdk
 {
     /// <summary>
     /// App information. To use the on-cloud 3D object reconstruction service, set an access token or API key for your app.
+    /// Wrapper for com.huawei.hms.objreconstructsdk.ReconstructApplication
+    /// <see cref="ReconstructApplication" link="https://developer.huawei.com/consumer/en/doc/development/graphics-References/reconstructapplication-0000001110787240"/>
     /// </summary>
     public class ReconstructApplication : JavaObjectWrapper
     {
         public ReconstructApplication(AndroidJavaObject javaObject) : base(javaObject)
         {
         }
-        //Not necessary to implement this because class name is the same as the java class name
-        //private static AndroidJavaClass sJavaClass = new AndroidJavaClass("com.huawei.hms.objreconstructsdk.ReconstructApplication");
+        private static AndroidJavaClass sJavaClass = new AndroidJavaClass("com.huawei.hms.objreconstructsdk.ReconstructApplication");
         public static explicit operator ReconstructApplication(AndroidJavaObject javaObject) => javaObject == null ? null : new ReconstructApplication(javaObject);
 
-        public string AppName => CallAsString("getAppName");
-        public ReconstructApplication GetInstance() => CallStatic<ReconstructApplication>("getInstance");
-        public void SetAccessToken (string accessToken) => Call("setAccessToken", accessToken);
-        public void SetApiKey (string apiKey) => Call("setApiKey", apiKey);
-        public void SetAppId (string appId) => Call("setAppId", appId);
+        public ReconstructApplication GetInstance() => sJavaClass.GetStaticAsWrapper<ReconstructApplication>("getInstance");
+        public void SetAccessToken (string accessToken) => Call("setAccessToken", accessToken.AsJavaString());
+        public void SetApiKey (string apiKey) => Call("setApiKey", apiKey.AsJavaString());
 
     }
 }
