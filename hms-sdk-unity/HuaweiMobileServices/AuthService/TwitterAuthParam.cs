0545cbfbbca0d8677a61018e132e9ee798cb5823
alihan98ersoy <alihan98ersoy@hotmail.com>
2024-02-28 17:38:17 +0300

    Update: Twitter credential with code
    credentialWithAuthCode(TwitterAuthParam twitterAuthParam, boolean autoCreateUser)
    function added to support Twitter account sign-in either in unified sign-in mode or based on the traditional OAuth 2.0 access mode.

---
+++ hms-sdk-unity/HuaweiMobileServices/AuthService/TwitterAuthParam.cs
@@ -0,0 +1,11 @@
+﻿using HuaweiMobileServices.Utils;
+using UnityEngine;
+
+namespace HuaweiMobileServices.AuthService
+{
+    public class TwitterAuthParam : JavaObjectWrapper
+    {
+        public TwitterAuthParam(AndroidJavaObject javaObject) : base(javaObject) { }
+        public TwitterAuthParam(string clientId, string authCode, string codeVerifier, string redirectUrl) : base("com.huawei.agconnect.auth.TwitterAuthParam", clientId, authCode, codeVerifier, redirectUrl) { }
+    }
+}
