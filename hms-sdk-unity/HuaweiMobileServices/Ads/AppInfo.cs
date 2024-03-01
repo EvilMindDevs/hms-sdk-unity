5f7a0215534ea1574b13aaea3bf3ce451e51c54a
Seyyid Yiğit <seyyid364@gmail.com>
2024-03-01 16:00:47 +0300

    [HUP-823] Feature: Added AppInfo and PromoteInfo classes and edited related class(NativeAd).

---
+++ hms-sdk-unity/HuaweiMobileServices/Ads/AppInfo.cs
@@ -0,0 +1,41 @@
+﻿using HuaweiMobileServices.Utils;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace HuaweiMobileServices.Ads
+{
+    // https://developer.huawei.com/consumer/en/doc/HMSCore-References/appinfo-0000001600190342
+    //https://developer.huawei.com/consumer/en/doc/HMSCore-References/nativead-0000001133473814#section16461225471
+    // wrapper for com.huawei.hms.ads.AppInfo
+    public class AppInfo : JavaObjectWrapper
+    {
+        public AppInfo(AndroidJavaObject javaObject) : base(javaObject) { }
+
+        public string AppName => Call<string>("getAppName");
+
+        public string AppDescription => Call<string>("getAppDesc");
+
+        public string AppIconUrl => Call<string>("getAppIconUrl");
+
+        public string AppDetailUrl => Call<string>("getAppDetailUrl");
+
+        public string VersionName => Call<string>("getVersionName");
+
+        public string DeveloperName => Call<string>("getDeveloperName");
+
+        public string DownloadUrl => Call<string>("getDownloadUrl");
+
+        public string SafeDownloadUrl => Call<string>("getSafeDownloadUrl");
+
+        public string PrivacyPolicyLink => Call<string>("getPrivacyLink");
+
+        public string PermissionUrl => Call<string>("getPermissionUrl");
+
+        public void ShowPrivacyPolicy() => Call("showPrivacyPolicy", AndroidContext.ActivityContext);
+
+        public void ShowPermissionPage() => Call("showPermissionPage", AndroidContext.ActivityContext);
+
+    }
+}
---
+++ hms-sdk-unity/HuaweiMobileServices/Ads/PromoteInfo.cs
@@ -0,0 +1,25 @@
+﻿using HuaweiMobileServices.Utils;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace HuaweiMobileServices.Ads
+{
+    //https://developer.huawei.com/consumer/en/doc/HMSCore-References/promoteinfo-0000001750234640
+    //https://developer.huawei.com/consumer/en/doc/HMSCore-References/nativead-0000001133473814#section6726104634610
+    // Wrapper for com.huawei.hms.ads.base.PromoteInfo
+    // com.huawei.openalliance.ad.beans.metadata.PromoteInfo
+    public class PromoteInfo : JavaObjectWrapper
+    {
+        public PromoteInfo(AndroidJavaObject javaObject) : base(javaObject) { }
+
+        /*Promotion subtype.
+          1-  Quick app.
+          2-  WeChat mini-program.
+        */
+        public int Type => Call<int>("getType");
+
+        public string Name => Call<string>("getName");
+    }
+}
