b3084c09fe05acbcaa48a061dc6baf3abbe6f51f
alihan98ersoy <alihan98ersoy@hotmail.com>
2024-03-20 10:05:20 +0300

    RewardVerifyConfig.Builder added

--- hms-sdk-unity/HuaweiMobileServices/Ads/RewardVerifyConfig.cs
+++ hms-sdk-unity/HuaweiMobileServices/Ads/RewardVerifyConfig.cs
@@ -1,19 +1,27 @@
 ﻿namespace HuaweiMobileServices.Ads
 {
+    using HuaweiMobileServices.Ads.InstallReferrer;
     using HuaweiMobileServices.Utils;
     using UnityEngine;
 
     // Wrapper for com.huawei.hms.ads.reward.RewardVerifyConfig
     public class RewardVerifyConfig : JavaObjectWrapper
     {
-
-        
         public RewardVerifyConfig(AndroidJavaObject javaObject) : base(javaObject) { }
 
         public virtual string Data => CallAsString("getData");
 
         public virtual string UserId => CallAsString("getUserId");
 
+        // Wrapper for com.huawei.hms.ads.reward.RewardVerifyConfig.Builder
+        public class Builder : JavaObjectWrapper
+        {
+            public Builder(AndroidJavaObject javaObject) : base(javaObject) { }
+            public Builder() : base("com.huawei.hms.ads.reward.RewardVerifyConfig$Builder") { }
+            public Builder SetData(string data) => CallAsWrapper<Builder>("setData", data);
+            public Builder SetUserId(string userId) => CallAsWrapper<Builder>("setUserId", userId);
+            public RewardVerifyConfig Build() => CallAsWrapper<RewardVerifyConfig>("build");
+        }
     }
 
 }
