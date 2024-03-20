31c17b29c94b63aea882e51d6a6ad304ab955f09
alihan98ersoy <alihan98ersoy@hotmail.com>
2024-03-20 15:04:34 +0300

    ServerStatus & ExecuteServerStatusQuery added

---
+++ hms-sdk-unity/HuaweiMobileServices/CloudDB/ServerStatus.cs
@@ -0,0 +1,17 @@
+﻿using HuaweiMobileServices.Utils;
+using UnityEngine;
+
+//com.huawei.agconnect.cloud.database.ServerStatus
+namespace HuaweiMobileServices.CloudDB
+{
+    public class ServerStatus : JavaObjectWrapper
+    {
+        public ServerStatus(AndroidJavaObject javaObject) : base(javaObject) { }
+
+        public long ServerTimestamp
+        {
+            get => Call<long>("getServerTimestamp");
+        }
+    }
+}
+
