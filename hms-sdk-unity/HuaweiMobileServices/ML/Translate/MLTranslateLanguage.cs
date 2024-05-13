1ec29a63e4fe47db35ac846aa0f8f1d30803e8d2
Seyyid Yiğit <seyyid364@gmail.com>
2024-05-13 11:39:08 +0300

    [HUP-856] Created base translation methods, but will implement some local and remote functions after creating related classes. - getLocalTranslator and getRemoteTranslator will be added.

---
+++ hms-sdk-unity/HuaweiMobileServices/ML/Translate/MLTranslateLanguage.cs
@@ -0,0 +1,32 @@
+﻿using HuaweiMobileServices.Base;
+using HuaweiMobileServices.Utils;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HuaweiMobileServices.ML.Translate
+{
+    // Wrapper for com.huawei.hms.mlsdk.translate.MLTranslateLanguage
+    // https://developer.huawei.com/consumer/en/doc/hiai-References/mltranslatelanguage-0000001051136104
+    public class MLTranslateLanguage: JavaObjectWrapper
+    {
+        const string CLASS_NAME = "com.huawei.hms.mlsdk.translate.MLTranslateLanguage";
+        public MLTranslateLanguage(AndroidJavaObject androidJavaObject) : base(androidJavaObject) { }
+        public MLTranslateLanguage() : base(CLASS_NAME) { }
+
+        public ITask<ISet<string>> GetCloudAllLanguagesAsync()
+        {
+            var javaTask = Call<AndroidJavaObject>("getCloudAllLanguages");
+            return new TaskWrapper<ISet<string>>(javaTask, AndroidJavaObjectExtensions.AsStringSet);
+        }
+        public ITask<ISet<string>> GetLocalAllLanguagesAsync()
+        {
+            var javaTask = Call<AndroidJavaObject>("getLocalAllLanguages");
+            return new TaskWrapper<ISet<string>>(javaTask, AndroidJavaObjectExtensions.AsStringSet);
+        }
+
+        public ISet<string> GetCloudAllLanguages() => Call<AndroidJavaObject>("syncGetCloudAllLanguages").AsStringSet();
+
+        public ISet<string> GetLocalAllLanguages() => Call<AndroidJavaObject>("syncGetLocalAllLanguages").AsStringSet();
+
+    }
+}
