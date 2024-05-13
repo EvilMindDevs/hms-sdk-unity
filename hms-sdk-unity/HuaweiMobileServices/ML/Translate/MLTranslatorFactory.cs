7333e36de92ef35b48986a63cda9b4adbefb0538
Seyyid Yiğit <seyyid364@gmail.com>
2024-05-13 11:39:08 +0300

    [HUP-856] Created base translation methods, but will implement some local and remote functions after creating related classes. - getLocalTranslator and getRemoteTranslator will be added.

---
+++ hms-sdk-unity/HuaweiMobileServices/ML/Translate/MLTranslatorFactory.cs
@@ -0,0 +1,25 @@
+﻿using HuaweiMobileServices.ML.Common;
+using HuaweiMobileServices.Utils;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace HuaweiMobileServices.ML.Translate
+{
+    // Wrapper for com.huawei.hms.mlsdk.translate.MLTranslatorFactory
+    // https://developer.huawei.com/consumer/en/doc/hiai-References/mltranslatorfactory-0000001050167536
+    public class MLTranslatorFactory: JavaObjectWrapper
+    {
+        const string CLASS_NAME = "com.huawei.hms.mlsdk.translate.MLTranslatorFactory";
+        private static readonly AndroidJavaClass sJavaClass = new AndroidJavaClass(CLASS_NAME);
+
+        public MLTranslatorFactory(AndroidJavaObject javaObject) : base(javaObject) { }
+
+        public static MLTranslatorFactory Instance => sJavaClass.CallStaticAsWrapper<MLTranslatorFactory>("getInstance");
+        public static MLTranslatorFactory GetInstance(MLApplication mLApplication) => sJavaClass.CallStaticAsWrapper<MLTranslatorFactory>("getInstance", mLApplication);
+
+        // Will be implemented in the future, GetLocalTranslator and GetRemoteTranslator will be added.
+
+    }
+}
