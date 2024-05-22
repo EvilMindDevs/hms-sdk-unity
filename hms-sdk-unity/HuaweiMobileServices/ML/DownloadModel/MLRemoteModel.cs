348893b6ad7ea821c95d33de706db527519b8734
Emine INAN <emineinanx@gmail.com>
2024-05-22 16:51:49 +0300

    Create MLRemoteModel class

---
+++ hms-sdk-unity/HuaweiMobileServices/ML/DownloadModel/MLRemoteModel.cs
@@ -0,0 +1,15 @@
+﻿using HuaweiMobileServices.Utils;
+using UnityEngine;
+
+namespace HuaweiMobileServices.ML.DownloadModel
+{
+    // Wrapper for com.huawei.hms.mlsdk.model.download.MLRemoteModel
+    // https://developer.huawei.com/consumer/en/doc/hiai-References/mlremotemodel-0000001050444221
+    public class MLRemoteModel : JavaObjectWrapper
+    {
+        public MLRemoteModel(AndroidJavaObject javaObject) : base(javaObject) { }
+
+        public override bool Equals(object other) => CallAsBool("equals", other);
+        public override int GetHashCode() => Call<int>("hashCode");
+    }
+}
