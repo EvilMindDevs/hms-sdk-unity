a298474bacbadf2524a375b842a98f5da0d72c5c
Emine INAN <emineinanx@gmail.com>
2024-05-22 16:51:07 +0300

    Create ML remote classes

---
+++ hms-sdk-unity/HuaweiMobileServices/ML/Translate/Cloud/MLRemoteTranslator.cs
@@ -0,0 +1,19 @@
+﻿using HuaweiMobileServices.Base;
+using HuaweiMobileServices.Utils;
+using UnityEngine;
+
+namespace HuaweiMobileServices.ML.Translate.Cloud
+{
+    // Wrapper for com.huawei.hms.mlsdk.translate.cloud.MLRemoteTranslator
+    // https://developer.huawei.com/consumer/en/doc/hiai-References/mlremotetranslator-0000001050167532
+    public class MLRemoteTranslator : JavaObjectWrapper
+    {
+        public MLRemoteTranslator(AndroidJavaObject javaObject) : base(javaObject) { }
+      
+        public ITask<string> TranslateAsync(string text) => CallAsWrapper<TaskPrimitive<string>>("asyncTranslate", text);
+
+        public void Stop() => Call("stop");
+
+        public string Translate(string text) => Call<string>("syncTranslate", text);
+    }
+}
