e83ff78f91ccc9d714d66726358003eb1ca403ff
Seyyid Yiğit <seyyid364@gmail.com>
2024-06-14 12:00:31 +0300

    [HUP-860] Fix: Add MLRemoteModel class for remote ML models
    - Fixed some bugs, wrong syntax and edited class relation.
    - The code changes introduce a new class, `MLRemoteModel`, which is responsible for handling remote ML models. This class is added to support the functionality of downloading and using ML models offline. The addition of this class is in line with the recent commits that have been made to create download model classes for offline use and local translation.

--- hms-sdk-unity/HuaweiMobileServices/ML/TextToSpeech/MLTtsConstants.cs
+++ hms-sdk-unity/HuaweiMobileServices/ML/TextToSpeech/MLTtsConstants.cs
@@ -20,9 +20,11 @@ namespace HuaweiMobileServices.ML.TextToSpeech
         public static int EVENT_SYNTHESIS_COMPLETE => sJavaClass.GetStatic<int>(nameof(EVENT_SYNTHESIS_COMPLETE));
         public static string EVENT_PLAY_STOP_INTERRUPTED => sJavaClass.GetStatic<string>(nameof(EVENT_PLAY_STOP_INTERRUPTED));
         public static string EVENT_SYNTHESIS_INTERRUPTED => sJavaClass.GetStatic<string>(nameof(EVENT_SYNTHESIS_INTERRUPTED));
+        
         public static int LANGUAGE_AVAILABLE => sJavaClass.GetStatic<int>(nameof(LANGUAGE_AVAILABLE));
         public static int LANGUAGE_NOT_SUPPORT => sJavaClass.GetStatic<int>(nameof(LANGUAGE_NOT_SUPPORT));
         public static int LANGUAGE_UPDATING => sJavaClass.GetStatic<int>(nameof(LANGUAGE_UPDATING));
+
         public static string TTS_EN_US => sJavaClass.GetStatic<string>(nameof(TTS_EN_US));
         public static string TTS_LAN_AR_AR => sJavaClass.GetStatic<string>(nameof(TTS_LAN_AR_AR));
         public static string TTS_LAN_TR_TR => sJavaClass.GetStatic<string>(nameof(TTS_LAN_TR_TR));
@@ -34,6 +36,7 @@ namespace HuaweiMobileServices.ML.TextToSpeech
         public static string TTS_LAN_PL_PL => sJavaClass.GetStatic<string>(nameof(TTS_LAN_PL_PL));
         public static string TTS_LAN_TH_TH => sJavaClass.GetStatic<string>(nameof(TTS_LAN_TH_TH));
         public static string TTS_LAN_MS_MS => sJavaClass.GetStatic<string>(nameof(TTS_LAN_MS_MS));
+
         public static string TTS_SPEAKER_FEMALE_EN => sJavaClass.GetStatic<string>(nameof(TTS_SPEAKER_FEMALE_EN));
         public static string TTS_SPEAKER_FEMALE_ZH_2 => sJavaClass.GetStatic<string>(nameof(TTS_SPEAKER_FEMALE_ZH_2));
         public static string TTS_SPEAKER_MALE_ZH_2 => sJavaClass.GetStatic<string>(nameof(TTS_SPEAKER_MALE_ZH_2));
@@ -53,19 +56,31 @@ namespace HuaweiMobileServices.ML.TextToSpeech
         public static string TTS_SPEAKER_FEMALE_TH => sJavaClass.GetStatic<string>(nameof(TTS_SPEAKER_FEMALE_TH));
         public static string TTS_SPEAKER_FEMALE_MS => sJavaClass.GetStatic<string>(nameof(TTS_SPEAKER_FEMALE_MS));
         public static string TTS_ZH_HANS => sJavaClass.GetStatic<string>(nameof(TTS_ZH_HANS));
+
         public static string TTS_OFFLINE_MODE => sJavaClass.GetStatic<string>(nameof(TTS_OFFLINE_MODE));
         public static string TTS_ONLINE_MODE => sJavaClass.GetStatic<string>(nameof(TTS_ONLINE_MODE));
+
+        public static string TTS_SPEAKER_OFFLINE_AR_AR_FEMALE_FLY => sJavaClass.GetStatic<string>(nameof(TTS_SPEAKER_OFFLINE_AR_AR_FEMALE_FLY));
         public static string TTS_SPEAKER_OFFLINE_DE_DE_FEMALE_BEE => sJavaClass.GetStatic<string>(nameof(TTS_SPEAKER_OFFLINE_DE_DE_FEMALE_BEE)); 
+        public static string TTS_SPEAKER_OFFLINE_DE_DE_FEMALE_FLY => sJavaClass.GetStatic<string>(nameof(TTS_SPEAKER_OFFLINE_DE_DE_FEMALE_FLY));
         public static string TTS_SPEAKER_OFFLINE_EN_US_FEMALE_BEE => sJavaClass.GetStatic<string>(nameof(TTS_SPEAKER_OFFLINE_EN_US_FEMALE_BEE));
         public static string TTS_SPEAKER_OFFLINE_EN_US_FEMALE_EAGLE => sJavaClass.GetStatic<string>(nameof(TTS_SPEAKER_OFFLINE_EN_US_FEMALE_EAGLE));
         public static string TTS_SPEAKER_OFFLINE_EN_US_MALE_EAGLE => sJavaClass.GetStatic<string>(nameof(TTS_SPEAKER_OFFLINE_EN_US_MALE_EAGLE));
+        public static string TTS_SPEAKER_OFFLINE_EN_US_FEMALE_BOLT => sJavaClass.GetStatic<string>(nameof(TTS_SPEAKER_OFFLINE_EN_US_FEMALE_BOLT));
+        public static string TTS_SPEAKER_OFFLINE_EN_US_MALE_BOLT => sJavaClass.GetStatic<string>(nameof(TTS_SPEAKER_OFFLINE_EN_US_MALE_BOLT));
+        public static string TTS_SPEAKER_OFFLINE_EN_US_FEMALE_FLY => sJavaClass.GetStatic<string>(nameof(TTS_SPEAKER_OFFLINE_EN_US_FEMALE_FLY));
         public static string TTS_SPEAKER_OFFLINE_ES_ES_FEMALE_BEE => sJavaClass.GetStatic<string>(nameof(TTS_SPEAKER_OFFLINE_ES_ES_FEMALE_BEE));
-        public static string TTS_SPEAKER_OFFLINE_FEMALE_DE => sJavaClass.GetStatic<string>(nameof(TTS_SPEAKER_OFFLINE_FEMALE_DE));
-        public static string TTS_SPEAKER_OFFLINE_FEMALE_IT => sJavaClass.GetStatic<string>(nameof(TTS_SPEAKER_OFFLINE_FEMALE_IT));
+        public static string TTS_SPEAKER_OFFLINE_ES_ES_FEMALE_FLY => sJavaClass.GetStatic<string>(nameof(TTS_SPEAKER_OFFLINE_ES_ES_FEMALE_FLY));
         public static string TTS_SPEAKER_OFFLINE_FR_FR_FEMALE_BEE => sJavaClass.GetStatic<string>(nameof(TTS_SPEAKER_OFFLINE_FR_FR_FEMALE_BEE));
+        public static string TTS_SPEAKER_OFFLINE_FR_FR_FEMALE_FLY => sJavaClass.GetStatic<string>(nameof(TTS_SPEAKER_OFFLINE_FR_FR_FEMALE_FLY));
         public static string TTS_SPEAKER_OFFLINE_IT_IT_FEMALE_BEE => sJavaClass.GetStatic<string>(nameof(TTS_SPEAKER_OFFLINE_IT_IT_FEMALE_BEE));
+        public static string TTS_SPEAKER_OFFLINE_IT_IT_FEMALE_FLY => sJavaClass.GetStatic<string>(nameof(TTS_SPEAKER_OFFLINE_IT_IT_FEMALE_FLY));
+        public static string TTS_SPEAKER_OFFLINE_RU_RU_FEMALE_FLY => sJavaClass.GetStatic<string>(nameof(TTS_SPEAKER_OFFLINE_RU_RU_FEMALE_FLY));
+        public static string TTS_SPEAKER_OFFLINE_TH_TH_FEMALE_FLY => sJavaClass.GetStatic<string>(nameof(TTS_SPEAKER_OFFLINE_TH_TH_FEMALE_FLY));
         public static string TTS_SPEAKER_OFFLINE_ZH_HANS_FEMALE_EAGLE => sJavaClass.GetStatic<string>(nameof(TTS_SPEAKER_OFFLINE_ZH_HANS_FEMALE_EAGLE));
         public static string TTS_SPEAKER_OFFLINE_ZH_HANS_MALE_EAGLE => sJavaClass.GetStatic<string>(nameof(TTS_SPEAKER_OFFLINE_ZH_HANS_MALE_EAGLE));
+        public static string TTS_SPEAKER_OFFLINE_ZH_HANS_FEMALE_BOLT => sJavaClass.GetStatic<string>(nameof(TTS_SPEAKER_OFFLINE_ZH_HANS_FEMALE_BOLT));
+        public static string TTS_SPEAKER_OFFLINE_ZH_HANS_MALE_BOLT => sJavaClass.GetStatic<string>(nameof(TTS_SPEAKER_OFFLINE_ZH_HANS_MALE_BOLT));
         #endregion  
     
 
